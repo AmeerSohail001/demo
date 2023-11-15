0606d4f1d9ccdfe77f284ec8e0c8a61fc777f9a6
Aameer Sohail <aameersohail001@gmail.com>
2023-11-15 14:30:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from '../src/pages/Home'
+import Navbar from '../src/component/navbar'
+
+function App() {
+  return (
+
+
+    <div className="App">
+      <Navbar/>
+    <Home/>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar=()=>{
+
+    return(
+        <>
+       <div className='nav-bar'>
+            <ul>
+                <li>
+                    <a href=''>Home</a>
+                </li>
+                <li>
+                    <a href=''>Home</a>
+                </li>
+                <li>
+                    <a href=''>Home</a>
+                </li>
+                <li>
+                    <a href=''>Home</a>
+                </li>
+                
+            </ul>
+        </div>
+        
+        </>
+    )
+}
+export default Navbar
